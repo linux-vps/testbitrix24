eeae3148ee783191bb3a628000b27dc3d8020996
dmhaui <dinhnhatminh.minhkhanh@gmail.com>
2024-08-14 04:20:42 +0700

    start script

---
+++ start.sh
@@ -0,0 +1,28 @@
+echo "rename all file .env.example to .env..."
+cd public
+mv .env.example .env
+cd ..
+cd server
+mv .env.example .env
+cd ..
+
+echo "Install dependencies and start the web app..."
+cd server
+echo "Installing dependencies for backend"
+npm install
+echo "Starting backend with pm2"
+pm2 start index.js --name "server"
+cd ..
+
+cd public
+echo "Installing dependencies for frontend"
+npm install
+echo "Building webpack"
+npm run build
+cd .. 
+echo "Starting frontend with pm2"
+pm2 start public/index.js --name "public"
+
+echo "Web app deployed! check the port to access the web."
+echo "Web running at http://eop.id.vn:5001"
+
