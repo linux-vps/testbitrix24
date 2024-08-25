49352cdf5717406e82ba4e91e3c4e8e6d6fee9dc
Đinh Nhật Minh <147995815+linux-vps@users.noreply.github.com>
2024-08-26 01:18:22 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,9 @@ echo "Installing dependencies for frontend"
 npm install
 echo "Building webpack"
 npm run build
-cd .. 
 echo "Starting frontend with pm2"
-pm2 start public/index.js --name "public"
+pm2 start index.js --name "public"
 
 echo "Web app deployed! check the port to access the web."
-echo "Web running at http://eop.id.vn:5001"
+
 
